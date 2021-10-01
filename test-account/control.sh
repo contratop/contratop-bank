575c8743e5390067fe88eda0f243d0c8dcb0de22
ContratopDev <Contratop@molecule.net>
2021-10-01 23:57:30 +0000

    FastPushed

--- test-account/control.sh
+++ test-account/control.sh
@@ -3,7 +3,55 @@
 
 
 
+## save credit.txt as variable
+testcredit=$(curl https://raw.githubusercontent.com/contratop/contratop-bank/main/test-account/credit.txt)
+
+
+echo "Fondos de test-account: " $testcredit
+echo ""
 
 
 
+## display transaction menu
 echo "Seleccione el tipo de transaccion"
+
+echo Que funcion vas a realizar
+	PS3='Operaciones para test-account: '
+options=("Agregar" "Retirar" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+
+
+
+
+
+
+        "Agregar")
+
+            read -p "Seleccione el monto a ingresar: " tempadd
+
+
+
+		
+				
+            ;;
+
+
+          "Retirar")
+
+
+
+
+
+
+         ;;
+
+
+        "Quit")
+		clear
+            exit
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
